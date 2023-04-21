73ea7336e89ed8944773c917f25714d255b0f3e2
Alex Barrett <akbarrett1993@gmail.com>
2023-04-21 13:56:38 -0400

    wireframe serverjs and indexjs added middleware

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+const api = require(".routes/index.js");
+
+const app = express();
+const PORT = 3001;
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api", api);
+app.use(express.static("public"));
+
+// Routes for the page
+app.get("/", (req, res) => res.sendFile(__dirname, "/public/index.html"));
+
+app.get("*", (req, res) => res.sendFile(__dirname, "/public/index.html"));
+
+app.get("/notes", (req, res) => res.sendFile(__dirname, "/public/notes.html"));
+
+app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
