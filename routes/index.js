3c77f0d4aed5ec8137b70b91c9ac8da7c28f9846
Alex Barrett <akbarrett1993@gmail.com>
2023-04-21 14:22:34 -0400

    utils uuid and routes added

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,29 @@
 const express = require("express");
+const { readFromFile, readAndAppend } = require("../helpers/fsUtils");
+const uuid = require("../helpers/uuid");
+const db = require("../db/db.json");
 
 const app = express();
+
+// Get route for all notes
+app.get("/notes", (req, res) => {
+  readFromFile(db).then((data) => res.json(json.parse(data)));
+});
+
+// Post route for submitting a note
+app.post("/notes", (req, res) => {
+  const { title, text } = req.body;
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      id: uuid(),
+    };
+    readAndAppend(newNote, "./db/db.json");
+    res.json("Note added.");
+  } else {
+    res.error("Error adding note.");
+  }
+});
 
 module.exports = app;
